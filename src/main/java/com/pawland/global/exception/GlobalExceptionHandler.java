b0ae49138fa45a5919439e82aaf9973cb6094f3b
midcondria <hyukkind@naver.com>
2024-04-13 13:59:48 +0900

    feat: 커스텀 예외 핸들러에 등록

--- src/main/java/com/pawland/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pawland/global/exception/GlobalExceptionHandler.java
@@ -1,9 +1,17 @@
 package com.pawland.global.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(PawLandException.class)
+    public ResponseEntity PawLandExceptionHandler(PawLandException e) {
 
+        return ResponseEntity
+            .status(e.getStatusCode())
+            .body(null);
+    }
 }
