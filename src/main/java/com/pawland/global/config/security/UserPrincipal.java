05f5bdadd32f58918ade907fadc6c7a0db640305
midcondria <hyukkind@naver.com>
2024-04-15 19:13:15 +0900

    feat: 시큐리티 커스텀 Principal 객체 추가

---
+++ src/main/java/com/pawland/global/config/security/UserPrincipal.java
@@ -0,0 +1,23 @@
+package com.pawland.global.config.security;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.List;
+
+public class UserPrincipal extends User {
+
+    private final Long userId;
+
+    public UserPrincipal(com.pawland.user.domain.User user) {
+        super(user.getEmail(), user.getPassword(),
+            List.of(
+                new SimpleGrantedAuthority("ROLE_USER")
+            ));
+        this.userId = user.getId();
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+}
