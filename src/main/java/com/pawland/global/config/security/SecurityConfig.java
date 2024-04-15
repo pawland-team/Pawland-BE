d7e768c009a9935f413c6bd73b5f7f747565e26b
midcondria <hyukkind@naver.com>
2024-04-15 19:58:44 +0900

    feat: 스프링 시큐리티 설정 적용

--- src/main/java/com/pawland/global/config/security/SecurityConfig.java
+++ src/main/java/com/pawland/global/config/security/SecurityConfig.java
@@ -1,11 +1,24 @@
 package com.pawland.global.config.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pawland.global.config.security.filter.EmailPasswordAuthFilter;
+import com.pawland.global.config.security.handler.LoginFailHandler;
+import com.pawland.global.config.security.handler.LoginSuccessHandler;
+import com.pawland.user.domain.User;
+import com.pawland.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,7 +27,13 @@ import static org.springframework.boot.autoconfigure.security.servlet.PathReques
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
+@Slf4j
 public class SecurityConfig {
+
+    private final ObjectMapper objectMapper;
+    private final UserRepository userRepository;
+    private final JwtUtils jwtUtils;
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
@@ -32,6 +51,32 @@ public class SecurityConfig {
             )
             .csrf(AbstractHttpConfigurer::disable)
             .build();
+    }
+
+    @Bean
+    public EmailPasswordAuthFilter usernamePasswordAuthenticationFilter() {
+        EmailPasswordAuthFilter filter = new EmailPasswordAuthFilter("/api/auth/login", objectMapper);
+        filter.setAuthenticationManager(authenticationManager());
+        filter.setAuthenticationSuccessHandler(new LoginSuccessHandler(objectMapper, jwtUtils));
+        filter.setAuthenticationFailureHandler(new LoginFailHandler(objectMapper));
+        return filter;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService(userRepository));
+        provider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService (UserRepository userRepository) {
+        return email -> {
+            User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(email + "을 찾을 수 없습니다."));
+            return new UserPrincipal(user);
+        };
     }
 
     @Bean
