2874333f8d7acef82053644528517ce77ed32029
midcondria <hyukkind@naver.com>
2024-04-17 13:03:53 +0900

    feat: BadCredential 예외 처리 추가

--- src/main/java/com/pawland/global/config/security/filter/JwtAuthFilter.java
+++ src/main/java/com/pawland/global/config/security/filter/JwtAuthFilter.java
@@ -6,12 +6,17 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+
+import static jakarta.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class JwtAuthFilter extends OncePerRequestFilter {
 
@@ -31,10 +36,17 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         Cookie[] cookies = request.getCookies();
-        String jwt = jwtUtils.getJwtFromCookie(cookies);
+        try {
+            String jwt = jwtUtils.getJwtFromCookie(cookies);
+            Authentication authentication = jwtUtils.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        Authentication authentication = jwtUtils.getAuthentication(jwt);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        filterChain.doFilter(request, response);
+            filterChain.doFilter(request, response);
+        } catch (BadCredentialsException e) {
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding(UTF_8.name());
+            response.setStatus(SC_UNAUTHORIZED);
+            response.getWriter().write(e.getMessage());
+        }
     }
 }
