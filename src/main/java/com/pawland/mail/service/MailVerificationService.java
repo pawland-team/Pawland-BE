c322a325ab10a510fb8b45d7f74965a9b051789f
midcondria <hyukkind@naver.com>
2024-04-22 03:36:44 +0900

    feat: 인증번호 발송 메일 저장 및 인증번호 검증 기능 구현

--- src/main/java/com/pawland/mail/service/MailVerificationService.java
+++ src/main/java/com/pawland/mail/service/MailVerificationService.java
@@ -1,29 +1,39 @@
 package com.pawland.mail.service;
 
 import com.pawland.global.config.MailConfig;
+import com.pawland.global.exception.InvalidCodeException;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.UnsupportedEncodingException;
+import java.time.Duration;
 import java.util.Random;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class MailVerificationService implements MailService {
+@Transactional(readOnly = true)
+public class MailVerificationService {
 
     private final MailConfig mailConfig;
     private final JavaMailSender mailSender;
+    private final RedisTemplate<String, String> redisTemplate;
 
-    @Override
-    public void sendEmail(String toEmail) throws MessagingException, UnsupportedEncodingException {
-        MimeMessage message = createEmailVerificationMessage(toEmail);
+    @Transactional
+    public void sendVerificationCode(String toEmail) throws MessagingException, UnsupportedEncodingException {
+        String verificationCode = generateVerificationCode();
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        values.set(toEmail, verificationCode, Duration.ofMinutes(3));
+        MimeMessage message = createMessage(toEmail, verificationCode);
         try {
             mailSender.send(message);
         } catch (RuntimeException e) {
@@ -32,30 +42,42 @@ public class MailVerificationService implements MailService {
         }
     }
 
-    private MimeMessage createEmailVerificationMessage(String toEmail) throws MessagingException, UnsupportedEncodingException {
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message);
-        helper.setFrom(mailConfig.getFromEmail(), "나는짱");
-        helper.setTo(toEmail);
-        helper.setSubject("PAWLAND 이메일 인증");
-        String template = createTemplate();
-        helper.setText(template, true);
-        return message;
-    }
-
-    private String createTemplate() {
-        String template = "<html><body>";
-        template += "<p>안녕하세요, PAWLAND입니다.</p>";
-        template += "<p>인증 번호는 아래와 같습니다:</p>";
-        template += "<h2>" + generateVerificationCode() + "</h2>";
-        template += "<p>이 인증 번호를 입력하여 인증을 완료해주세요.</p>";
-        template += "</body></html>";
-        return template;
+    public void verifyCode(String toEmail, String code) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        String savedVerificationCode = values.get(toEmail);
+        boolean isVerified = code.equals(savedVerificationCode);
+        if (isVerified) {
+            values.set(toEmail, "ok", Duration.ofMinutes(5));
+        } else {
+            log.error("[메일 인증 실패]");
+            throw new InvalidCodeException();
+        }
     }
 
     private String generateVerificationCode() {
         Random random = new Random();
         int randomNumber = random.nextInt(999999) + 1;
         return String.format("%06d", randomNumber);
+    }
+
+    private MimeMessage createMessage(String toEmail, String verificationCode) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+        helper.setFrom(mailConfig.getFromEmail(), "나는짱");
+        helper.setTo(toEmail);
+        helper.setSubject("PAWLAND 이메일 인증");
+        String template = createTemplate(verificationCode);
+        helper.setText(template, true);
+        return message;
+    }
+
+    private String createTemplate(String verificationCode) {
+        String template = "<html><body>";
+        template += "<p>안녕하세요, PAWLAND입니다.</p>";
+        template += "<p>인증 번호는 아래와 같습니다:</p>";
+        template += "<h2>" + verificationCode + "</h2>";
+        template += "<p>이 인증 번호를 입력하여 인증을 완료해주세요.</p>";
+        template += "</body></html>";
+        return template;
     }
 }
