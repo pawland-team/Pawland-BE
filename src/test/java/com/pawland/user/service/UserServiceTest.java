21f9973ed4ea6f8939560258786fee89c743bbdb
midcondria <hyukkind@naver.com>
2024-04-24 17:20:00 +0900

    feat: 회원 단건 조회 구현

--- src/test/java/com/pawland/user/service/UserServiceTest.java
+++ src/test/java/com/pawland/user/service/UserServiceTest.java
@@ -1,18 +1,26 @@
 package com.pawland.user.service;
 
+import com.pawland.global.config.AppConfig;
 import com.pawland.global.exception.AlreadyExistsUserException;
 import com.pawland.user.domain.User;
+import com.pawland.user.dto.response.UserInfoResponse;
+import com.pawland.user.exception.UserException;
 import com.pawland.user.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static com.pawland.user.exception.UserExceptionMessage.USER_NOT_FOUND;
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class UserServiceTest {
+
+    @Autowired
+    private AppConfig appConfig;
 
     @Autowired
     private UserService userService;
@@ -53,47 +61,100 @@ class UserServiceTest {
             .hasMessageContaining("이미 존재하는 유저입니다.");
     }
 
-    @DisplayName("정상적인 정보 입력 시 중복 이메일이 아니라면 유저 정보를 DB에 저장한다.")
-    @Test
-    void register1() {
-        // given
-        User user = User.builder()
-            .email("mid@nav.com")
-            .password("asd123123")
-            .nickname("나는짱")
-            .build();
+    @DisplayName("유저 등록 시")
+    @Nested
+    class register {
+        @DisplayName("정상적인 정보 입력 시 중복 이메일이 아니라면 유저 정보를 DB에 저장한다.")
+        @Test
+        void register1() {
+            // given
+            User user = User.builder()
+                .email("mid@nav.com")
+                .password("asd123123")
+                .nickname("나는짱")
+                .build();
 
-        // when
-        userService.register(user);
-        User result = userRepository.findByEmail(user.getEmail())
-            .orElseThrow(IllegalArgumentException::new);
+            // when
+            userService.register(user);
+            User result = userRepository.findByEmail(user.getEmail())
+                .orElseThrow(IllegalArgumentException::new);
 
-        // then
-        assertThat(result.getEmail()).isEqualTo(user.getEmail());
-        assertThat(result.getNickname()).isEqualTo(user.getNickname());
-        assertThat(result.getNickname()).isEqualTo(user.getNickname());
+            // then
+            assertThat(result.getEmail()).isEqualTo(user.getEmail());
+            assertThat(result.getNickname()).isEqualTo(user.getNickname());
+        }
+
+        @DisplayName("정상적인 정보 입력 시 중복 이메일이 존재하면 실패한다.")
+        @Test
+        void register2() {
+            // given
+            User user = User.builder()
+                .email("mid@nav.com")
+                .password("asd123123")
+                .nickname("나는짱")
+                .build();
+            userService.register(user);
+
+            User duplicateEmailUser = User.builder()
+                .email("mid@nav.com")
+                .password("asd123123")
+                .nickname("나는짱")
+                .build();
+
+            // expected
+            assertThatThrownBy(() -> userService.register(duplicateEmailUser))
+                .isInstanceOf(AlreadyExistsUserException.class)
+                .hasMessageContaining("이미 존재하는 유저입니다.");
+        }
     }
 
-    @DisplayName("정상적인 정보 입력 시 중복 이메일이 존재하면 실패한다.")
-    @Test
-    void register2() {
-        // given
-        User user = User.builder()
-            .email("mid@nav.com")
-            .password("asd123123")
-            .nickname("나는짱")
-            .build();
+    @DisplayName("유저 정보 단건 조회 시")
+    @Nested
+    class getUserInfo {
+        @DisplayName("유저 정보 조회에 성공한다.")
+        @Test
+        void getUserInfo1() {
+            // given
+            User user = User.builder()
+                .email("mid@nav.com")
+                .password("asd123123")
+                .nickname("나는짱")
+                .introduce("나아는짱")
+                .profileImage(appConfig.getDefaultImage())
+                .build();
+            userRepository.save(user);
 
-        userService.register(user);
-        User duplicateUser = User.builder()
-            .email("mid@nav.com")
-            .password("asd123123")
-            .nickname("나는짱")
-            .build();
+            String toGetEmail = "mid@nav.com";
+            double tempStars = 3.5;  // TODO 평점 조회 로직 구현시 수정 필요
 
-        // expected
-        assertThatThrownBy(() -> userService.register(duplicateUser))
-            .isInstanceOf(AlreadyExistsUserException.class)
-            .hasMessageContaining("이미 존재하는 유저입니다.");
+            // when
+            UserInfoResponse result = userService.getUserInfo(toGetEmail);
+
+            // then
+            assertThat(result.getId()).isNotNull();
+            assertThat(result.getEmail()).isEqualTo(user.getEmail());
+            assertThat(result.getNickname()).isEqualTo(user.getNickname());
+            assertThat(result.getUserDesc()).isEqualTo(user.getIntroduce());
+            assertThat(result.getStars()).isEqualTo(tempStars);
+            assertThat(result.getProfileImage()).isNotBlank();
+    }
+
+        @DisplayName("DB에 저장되지 않은 유저를 조회 시도 시 실패한다.")
+        @Test
+        void getUserInfo2() {
+            User user = User.builder()
+                .email("mid@nav.com")
+                .password("asd123123")
+                .nickname("나는짱")
+                .build();
+            userRepository.save(user);
+
+            String toGetEmail = "midcon@nav.com";
+
+            // expected
+            assertThatThrownBy(() -> userService.getUserInfo(toGetEmail))
+                .isInstanceOf(UserException.NotFoundUser.class)
+                .hasMessageContaining(USER_NOT_FOUND.getMessage());
+        }
     }
 }
