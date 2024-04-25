4c7f4d6f5cc4fc8a25a977ef1df8254e81f1aaee
cmong0516 <74821906+cmong0516@users.noreply.github.com>
2024-04-25 14:00:54 +0900

    Update OrderServiceTest.java

--- src/test/java/com/pawland/order/service/OrderServiceTest.java
+++ src/test/java/com/pawland/order/service/OrderServiceTest.java
@@ -14,12 +14,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@Transactional
 class OrderServiceTest {
 
     @Autowired
