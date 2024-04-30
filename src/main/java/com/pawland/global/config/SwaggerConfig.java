6151cf3fcd11e82d3bf5f702d41b8d05e19e9740
midcondria <hyukkind@naver.com>
2024-04-30 13:07:48 +0900

    refactor: 스웨거 설정 URL도 환경 변수로 분리

--- src/main/java/com/pawland/global/config/SwaggerConfig.java
+++ src/main/java/com/pawland/global/config/SwaggerConfig.java
@@ -6,12 +6,14 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 @SecurityScheme(
     name = "jwt-cookie",
     type = SecuritySchemeType.APIKEY,
@@ -19,10 +21,12 @@ import java.util.List;
 )
 public class SwaggerConfig {
 
+    private final AppConfig appConfig;
+
     @Bean
     public OpenAPI serverApiConfig() {
         Server server = new Server();
-        server.setUrl("https://www.midcon.store");
+        server.setUrl(appConfig.getBackUrl());
         server.description("백엔드 도메인");
         return new OpenAPI()
             .servers(List.of(server))
