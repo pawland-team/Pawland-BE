0047187d38dd5d80c16523da3383f9b15cc05b86
midcondria <hyukkind@naver.com>
2024-05-05 22:30:17 +0900

    feat: 스웨거용 DTO 설명 추가

--- src/main/java/com/pawland/image/dto/request/ImageUploadRequest.java
+++ src/main/java/com/pawland/image/dto/request/ImageUploadRequest.java
@@ -1,5 +1,6 @@
 package com.pawland.image.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(name = "Presigned URL 발급 요청")
 public class ImageUploadRequest {
 
     @NotBlank(message = "파일명을 확인해주세요.")
--- src/main/java/com/pawland/image/dto/response/ImageUploadResponse.java
+++ src/main/java/com/pawland/image/dto/response/ImageUploadResponse.java
@@ -1,8 +1,10 @@
 package com.pawland.image.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
+@Schema(name = "Presigned URL 발급 응답")
 public class ImageUploadResponse {
 
     private String presignedUrl;
