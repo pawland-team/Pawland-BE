1e59a273ceadc3b7140f081155257b8de4d5a001
midcondria <hyukkind@naver.com>
2024-05-09 03:01:00 +0900

    refactor: 유저 정보 조회/수정 응답 DTO 스웨거 스키마 추가

--- src/main/java/com/pawland/user/dto/response/UserInfoResponse.java
+++ src/main/java/com/pawland/user/dto/response/UserInfoResponse.java
@@ -1,22 +1,19 @@
 package com.pawland.user.dto.response;
 
 import com.pawland.user.domain.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Schema(name = "유저 정보 조회 응답")
 public class UserInfoResponse {
 
     private Long id;
-
     private String profileImage;
-
     private String nickname;
-
     private String email;
-
     private String userDesc;
-
     private double stars = 0.0; // 구현중
 
     @Builder
--- src/main/java/com/pawland/user/dto/response/UserInfoUpdateResponse.java
+++ src/main/java/com/pawland/user/dto/response/UserInfoUpdateResponse.java
@@ -1,18 +1,16 @@
 package com.pawland.user.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-
+@Schema(name = "유저 정보 수정 응답")
 public class UserInfoUpdateResponse {
 
     private Long id;
-
     private String profileImage;
-
     private String nickname;
-
     private String userDesc;
 
     @Builder
