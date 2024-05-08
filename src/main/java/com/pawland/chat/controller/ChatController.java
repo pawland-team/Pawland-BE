08365dbcb673d84806a0286ec9344109020c6e01
midcondria <hyukkind@naver.com>
2024-05-09 01:47:28 +0900

    feat: 내 채팅방 목록 조회 엔드포인트 추가

--- src/main/java/com/pawland/chat/controller/ChatController.java
+++ src/main/java/com/pawland/chat/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.pawland.chat.controller;
 
 import com.pawland.chat.dto.request.ChatRoomCreateRequest;
+import com.pawland.chat.dto.response.ChatRoomInfoResponse;
 import com.pawland.chat.service.ChatService;
 import com.pawland.global.config.security.domain.UserPrincipal;
 import com.pawland.global.dto.ApiMessageResponse;
@@ -13,7 +14,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @Slf4j
 @RestController
@@ -22,6 +26,16 @@ import static org.springframework.http.HttpStatus.CREATED;
 public class ChatController {
 
     private final ChatService chatService;
+
+    @Operation(summary = "내 채팅 목록 조회", description = "내 채팅 목록을 반환합니다.(미완성)")
+    @ApiResponse(responseCode = "200", description = "채팅 목록 조회 성공")
+    @GetMapping("/rooms")
+    public ResponseEntity<List<ChatRoomInfoResponse>> getChatRoomList(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+        List<ChatRoomInfoResponse> chatRoomList = chatService.getChatRoomList(userPrincipal.getUserId());
+        return ResponseEntity
+            .status(OK)
+            .body(chatRoomList);
+    }
 
     @Operation(summary = "채팅방 생성", description = "채팅방을 생성합니다.(미완성)")
     @ApiResponse(responseCode = "201", description = "채팅방 생성 성공")
--- src/test/java/com/pawland/chat/controller/ChatControllerTest.java
+++ src/test/java/com/pawland/chat/controller/ChatControllerTest.java
@@ -3,12 +3,14 @@ package com.pawland.chat.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pawland.chat.domain.ChatRoom;
 import com.pawland.chat.dto.request.ChatRoomCreateRequest;
+import com.pawland.chat.dto.response.ChatRoomInfoResponse;
 import com.pawland.chat.repository.ChatRoomRepository;
 import com.pawland.global.config.TestSecurityConfig;
 import com.pawland.global.utils.PawLandMockUser;
 import com.pawland.product.domain.Product;
 import com.pawland.product.respository.ProductJpaRepository;
 import com.pawland.user.domain.User;
+import com.pawland.user.exception.UserException;
 import com.pawland.user.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -21,9 +23,12 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static com.pawland.product.exception.ProductExceptionMessage.PRODUCT_NOT_FOUND;
 import static com.pawland.user.exception.UserExceptionMessage.USER_NOT_FOUND;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -140,6 +145,73 @@ class ChatControllerTest {
                 .andDo(print())
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message").value(USER_NOT_FOUND.getMessage()));
+        }
+    }
+
+    @DisplayName("내 채팅방 목록 조회 시")
+    @Nested
+    class getMyChatRoomList {
+        @DisplayName("내가 구매자인 채팅방과 판매자인 채팅방을 모두 조회한다.")
+        @PawLandMockUser
+        @Test
+        void getMyChatRoomList1() throws Exception {
+            // given
+            User seller1 = createUser("판매자1", "midcon2@naver.com", "asd123123");
+            User seller2 = createUser("판매자2", "midcon3@naver.com", "asd123123");
+            User buyer1 = createUser("구매자1", "midcon4@naver.com", "asd123123");
+            User buyer2 = createUser("구매자2", "midcon5@naver.com", "asd123123");
+            userRepository.saveAll(List.of(seller1, seller2, buyer1, buyer2));
+
+            User myAccount = userRepository.findByEmail("midcondria@naver.com")
+                .orElseThrow(UserException.NotFoundUser::new);
+
+            Product product1 = createProduct("나는짱물건1", 1000, "장난감", "DOG", "NEW");
+            Product product2 = createProduct("나는짱물건2", 2000, "장난감", "DOG", "NEW");
+            Product product3 = createProduct("나는짱물건3", 3000, "장난감", "DOG", "NEW");
+            Product product4 = createProduct("나는짱물건4", 4000, "장난감", "DOG", "NEW");
+            product3.confirmPurchase(1L);
+            productJpaRepository.saveAll(List.of(product1, product2, product3, product4));
+
+            ChatRoom myChatRoom1 = createChatRoom(myAccount.getId(), seller1.getId(), product1.getId());
+            ChatRoom myChatRoom2 = createChatRoom(myAccount.getId(), seller2.getId(), product2.getId());
+            ChatRoom myChatRoom3 = createChatRoom(buyer1.getId(), myAccount.getId(), product3.getId());
+            ChatRoom notMyChatRoom1 = createChatRoom(buyer1.getId(), seller2.getId(), product4.getId());
+            ChatRoom notMyChatRoom2 = createChatRoom(buyer2.getId(), seller2.getId(), product4.getId());
+            chatRoomRepository.saveAll(List.of(myChatRoom1, myChatRoom2, myChatRoom3, notMyChatRoom1, notMyChatRoom2));
+
+            // expected
+            mockMvc.perform(get("/api/chat/rooms"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(3));
+        }
+
+        @DisplayName("내가 참여하고 있는 채팅방이 없을 시 빈 리스트를 반환한다.")
+        @PawLandMockUser
+        @Test
+        void getMyChatRoomList2() throws Exception {
+            User seller1 = createUser("판매자1", "midcon2@naver.com", "asd123123");
+            User seller2 = createUser("판매자2", "midcon3@naver.com", "asd123123");
+            User buyer1 = createUser("구매자1", "midcon4@naver.com", "asd123123");
+            User buyer2 = createUser("구매자2", "midcon5@naver.com", "asd123123");
+            userRepository.saveAll(List.of(seller1, seller2, buyer1, buyer2));
+
+            User myAccount = userRepository.findByEmail("midcondria@naver.com")
+                .orElseThrow(UserException.NotFoundUser::new);
+
+            Product product1 = createProduct("나는짱물건1", 1000, "장난감", "DOG", "NEW");
+            Product product2 = createProduct("나는짱물건2", 2000, "장난감", "DOG", "NEW");
+            productJpaRepository.saveAll(List.of(product1, product2));
+
+            ChatRoom notMyChatRoom1 = createChatRoom(buyer1.getId(), seller1.getId(), product1.getId());
+            ChatRoom notMyChatRoom2 = createChatRoom(buyer2.getId(), seller2.getId(), product2.getId());
+            chatRoomRepository.saveAll(List.of(notMyChatRoom1, notMyChatRoom2));
+
+            // expected
+            mockMvc.perform(get("/api/chat/rooms"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isEmpty());
         }
     }
 
