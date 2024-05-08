e9a414a0564d62ab32643abafe9ffc24e41f14ba
midcondria <hyukkind@naver.com>
2024-05-09 02:32:21 +0900

    refactor: 유저 정보 수정 요청 DTO 필드명 변경

--- src/main/java/com/pawland/user/dto/request/UserInfoUpdateRequest.java
+++ src/main/java/com/pawland/user/dto/request/UserInfoUpdateRequest.java
@@ -1,6 +1,7 @@
 package com.pawland.user.dto.request;
 
 import com.pawland.user.domain.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -9,26 +10,27 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(name = "유저 정보 수정 요청")
 public class UserInfoUpdateRequest {
 
     @NotBlank(message = "닉네임을 입력해주세요.")
     private String nickname;
 
-    private String introduce = "";
+    private String userDesc = "";
 
     private String profileImage = "";
 
     @Builder
-    public UserInfoUpdateRequest(String nickname, String introduce, String profileImage) {
+    public UserInfoUpdateRequest(String nickname, String userDesc, String profileImage) {
         this.nickname = nickname;
-        this.introduce = introduce;
+        this.userDesc = userDesc;
         this.profileImage = profileImage;
     }
 
     public User toUser() {
         return User.builder()
             .nickname(nickname)
-            .introduce(introduce)
+            .introduce(userDesc)
             .profileImage(profileImage)
             .build();
     }
