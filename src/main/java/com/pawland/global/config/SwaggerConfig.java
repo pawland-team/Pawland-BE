0bd4370f90b07d1fd25177c54380746ac85a017e
midcondria <hyukkind@naver.com>
2024-05-15 15:16:28 +0900

    feat: 스웨거 ui에 인증 필요 표시 지우기 위한 설정 추가

--- src/main/java/com/pawland/global/config/SwaggerConfig.java
+++ src/main/java/com/pawland/global/config/SwaggerConfig.java
@@ -1,15 +1,20 @@
 package com.pawland.global.config;
 
+import com.pawland.global.config.swagger.SecurityNotRequired;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerMethod;
 
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -32,5 +37,17 @@ public class SwaggerConfig {
             .servers(List.of(server))
             .info(new Info().title("PAWLAND API")
                 .description("PAWLAND API SWAGGER UI입니다."));
+    }
+
+    @Bean
+    public OperationCustomizer customize() {
+        return (Operation operation, HandlerMethod handlerMethod) -> {
+            SecurityNotRequired annotation = handlerMethod.getMethodAnnotation(SecurityNotRequired.class);
+            // SecurityNotRequire 어노테이션있을시 스웨거 시큐리티 설정 삭제
+            if (annotation != null) {
+                operation.security(Collections.emptyList());
+            }
+            return operation;
+        };
     }
 }
---
+++ src/main/java/com/pawland/global/config/swagger/SecurityNotRequired.java
@@ -0,0 +1,11 @@
+package com.pawland.global.config.swagger;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SecurityNotRequired {
+}
