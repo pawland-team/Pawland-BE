a9644c6eba3344668d37f071122077b0eca54120
midcondria <hyukkind@naver.com>
2024-05-15 21:07:35 +0900

    feat: 로그아웃 엔드포인트 추가

--- src/main/java/com/pawland/auth/controller/AuthController.java
+++ src/main/java/com/pawland/auth/controller/AuthController.java
@@ -4,10 +4,12 @@ import com.pawland.auth.dto.request.*;
 import com.pawland.auth.facade.AuthFacade;
 import com.pawland.global.config.AppConfig;
 import com.pawland.global.config.security.domain.LoginRequest;
+import com.pawland.global.config.security.domain.UserPrincipal;
 import com.pawland.global.dto.ApiMessageResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.headers.Header;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
@@ -17,6 +19,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
@@ -41,8 +45,8 @@ public class AuthController {
     public ResponseEntity<ApiMessageResponse> nicknameDupCheck(@Valid @RequestBody NicknameDupCheckRequest request) {
         authFacade.checkNicknameDuplicate(request.getNickname());
         return ResponseEntity
-                .status(OK)
-                .body(new ApiMessageResponse("사용할 수 있는 닉네임입니다."));
+            .status(OK)
+            .body(new ApiMessageResponse("사용할 수 있는 닉네임입니다."));
     }
 
     @Operation(summary = "이메일 중복 확인", description = "요청한 이메일이 이미 가입된 이메일인지 확인합니다.")
@@ -52,8 +56,8 @@ public class AuthController {
     public ResponseEntity<ApiMessageResponse> emailDupCheck(@Valid @RequestBody EmailDupCheckRequest request) {
         authFacade.checkEmailDuplicate(request.getEmail());
         return ResponseEntity
-                .status(OK)
-                .body(new ApiMessageResponse("사용할 수 있는 이메일입니다."));
+            .status(OK)
+            .body(new ApiMessageResponse("사용할 수 있는 이메일입니다."));
     }
 
     @Operation(summary = "이메일로 인증번호 요청", description = "요청한 메일 주소로 인증번호가 담긴 메일을 발송 합니다.")
@@ -63,8 +67,8 @@ public class AuthController {
     public ResponseEntity<ApiMessageResponse> sendVerificationCode(@Valid @RequestBody SendVerificationCodeRequest request) throws MessagingException, UnsupportedEncodingException {
         authFacade.sendVerificationCode(request.getEmail());
         return ResponseEntity
-                .status(CREATED)
-                .body(new ApiMessageResponse("인증 메일이 발송 되었습니다."));
+            .status(CREATED)
+            .body(new ApiMessageResponse("인증 메일이 발송 되었습니다."));
     }
 
     @Operation(summary = "발급된 인증번호로 이메일 인증", description = "이메일과 인증번호를 확인하여 이메일을 인증합니다.")
@@ -74,49 +78,68 @@ public class AuthController {
     public ResponseEntity<ApiMessageResponse> verifyCode(@Valid @RequestBody VerifyCodeRequest request) {
         authFacade.verifyCode(request);
         return ResponseEntity
-                .status(OK)
-                .body(new ApiMessageResponse("이메일 인증이 완료되었습니다."));
+            .status(OK)
+            .body(new ApiMessageResponse("이메일 인증이 완료되었습니다."));
     }
 
     @Operation(summary = "회원가입", description = "회원가입 성공 시 쿠키를 반환합니다.")
     @ApiResponse(responseCode = "201", description = "회원가입 성공",
-            headers = {
-                    @Header(name = "Set-Cookie", description = "인증 쿠키")
-            })
+        headers = {
+            @Header(name = "Set-Cookie", description = "인증 쿠키")
+        })
     @PostMapping(value = "/signup", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ApiMessageResponse> signup(@Valid @RequestBody SignupRequest request) {
         String jwtCookie = authFacade.signup(request);
 
         return ResponseEntity
-                .status(CREATED)
-                .header(HttpHeaders.SET_COOKIE, jwtCookie)
-                .body(new ApiMessageResponse("회원가입 되었습니다."));
+            .status(CREATED)
+            .header(HttpHeaders.SET_COOKIE, jwtCookie)
+            .body(new ApiMessageResponse("회원가입 되었습니다."));
     }
 
     @Operation(summary = "로그인", description = "로그인 성공 시 쿠키를 반환합니다.")
     @ApiResponse(responseCode = "200", description = "로그인 성공",
-            headers = {
-                    @Header(name = "Set-Cookie", description = "인증 쿠키")
-            })
+        headers = {
+            @Header(name = "Set-Cookie", description = "인증 쿠키")
+        })
     @ApiResponse(responseCode = "400", description = "잘못된 아이디 혹은 비밀번호")
     @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ApiMessageResponse> login(@Valid @RequestBody LoginRequest request) {
-        return ResponseEntity.ok(new ApiMessageResponse(""));
+        return ResponseEntity
+            .status(OK)
+            .body(new ApiMessageResponse(""));
     }
 
     @Operation(summary = "소셜 로그인", description = "소셜 로그인 성공 시 쿠키를 반환합니다.")
     @ApiResponse(responseCode = "200", description = "로그인에 성공",
-            headers = {
-                    @Header(name = "Set-Cookie", description = "인증 쿠키")
-            })
+        headers = {
+            @Header(name = "Set-Cookie", description = "인증 쿠키")
+        })
     @ApiResponse(responseCode = "400", description = "잘못된 아이디 혹은 비밀번호")
     @GetMapping("/oauth2/{provider}")
     public ResponseEntity<ApiMessageResponse> oauth2Login(@PathVariable String provider, @RequestParam String code) {
         String jwtCookie = authFacade.oauth2Login(code, provider);
         return ResponseEntity
-                .status(HttpStatus.FOUND)
-                .header(HttpHeaders.SET_COOKIE, jwtCookie)
-                .header(HttpHeaders.LOCATION, appConfig.getFrontTestUrl())
-                .body(new ApiMessageResponse("소셜 로그인에 성공했습니다."));
+            .status(HttpStatus.FOUND)
+            .header(HttpHeaders.SET_COOKIE, jwtCookie)
+            .header(HttpHeaders.LOCATION, appConfig.getFrontTestUrl())
+            .body(new ApiMessageResponse("소셜 로그인에 성공했습니다."));
+    }
+
+    @SecurityRequirement(name = "jwt-cookie")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @Operation(summary = "로그아웃", description = "로그아웃 성공 시 쿠키를 제거합니다.")
+    @ApiResponse(responseCode = "200", description = "로그아웃 성공",
+        headers = {
+            @Header(name = "Set-Cookie", description = "인증 만료 된 쿠키")
+        })
+    @ApiResponse(responseCode = "401", description = "로그인 필요")
+    @PostMapping(value = "/logout", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiMessageResponse> logout(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+        String expiredJwtCookie = authFacade.logout(userPrincipal.getUsername());
+        return ResponseEntity
+            .status(OK)
+            .header(HttpHeaders.SET_COOKIE, expiredJwtCookie)
+            .body(new ApiMessageResponse("로그아웃 되었습니다."));
     }
 }
