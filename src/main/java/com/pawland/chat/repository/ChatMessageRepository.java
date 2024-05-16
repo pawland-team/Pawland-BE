c988ef8606df031295bfb2f6b3b3913b535d42b1
midcondria <hyukkind@naver.com>
2024-05-17 05:16:27 +0900

    feat: 각 채팅방의 마지막 메시지 조회 로직 추가

--- src/main/java/com/pawland/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/pawland/chat/repository/ChatMessageRepository.java
@@ -2,7 +2,13 @@ package com.pawland.chat.repository;
 
 import com.pawland.chat.domain.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long>, ChatMessageRepositoryCustom {
 
+    @Query(value = "SELECT * FROM chat_message cm WHERE cm.room_id = :roomId ORDER BY message_time DESC LIMIT 1", nativeQuery = true)
+    Optional<ChatMessage> getLastMessage(@Param("roomId") Long roomId);
 }
